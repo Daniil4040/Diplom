a68fbeb9299391d3cd3e48fcc79afb5e5bf36388
Daniil4040 <danya.bessonov.2019@mail.ru>
2022-11-13 17:02:12 +0700

    add seed

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,5 @@
 class Film < ApplicationRecord
     belongs_to :category
     has_many :comments
+    has_one_attached :poster
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category_film = Category.create!(name: "Фильмы")
+category_serial = Category.create!(name: "Сериалы")
+film = Film.create!(
+    name: 'Matrix',
+    trailer_link: 'https://www.youtube.com/embed/YihPA42fdQ8',
+    rating: 7.5,
+    year: 2001,
+    director: 'Tomac',
+    description: 'иллюзия, существующая только в бесконечном сне обреченного человечества. Холодный мир будущего, в котором люди — всего лишь батарейки в компьютерных системах.',
+    category: category_film
+)
+film.poster.attach(
+  io: File.open(Rails.root.join("db","matrix.png")),
+  filename: 'matrix.png',
+  content_type: 'application/png',
+  identify: false
+)
